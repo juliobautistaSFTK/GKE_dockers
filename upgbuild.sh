8f5d716a54fc9eef08a997a4b6ef96b20bb6a6a7
juliobautistaSFTK <84602723+juliobautistaSFTK@users.noreply.github.com>
2025-05-13 15:17:44 -0600

    Create upgbuild.sh

---
+++ upgbuild.sh
@@ -0,0 +1,7 @@
+
+docker network create red1
+sleep 20
+docker build -t maven-java8 .
+sleep 20
+docker run -d --name maven-java8-container -v ./target:/app/target --network red1 maven-java8 mvn install
+sleep 6
