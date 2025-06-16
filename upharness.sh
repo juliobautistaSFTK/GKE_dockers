108a5e71bb82b77bce5040c18da56419fc8af3b7
juliobautistaSFTK <84602723+juliobautistaSFTK@users.noreply.github.com>
2025-06-16 16:07:22 -0600

    Create upharness.sh

---
+++ upharness.sh
@@ -0,0 +1,20 @@
+rm TaskManager.war 
+sudo rm target/TaskManager.war
+
+gcloud container clusters get-credentials autopilot-cluster-1 --region us-central1
+
+gcloud projects add-iam-policy-binding maximal-relic-457716-k6 \
+  --member="serviceAccount:cloudsql-access@maximal-relic-457716-k6.iam.gserviceaccount.com" \
+  --role="roles/logging.logWriter" \
+  --condition=None
+
+gcloud projects add-iam-policy-binding maximal-relic-457716-k6 \
+  --member="serviceAccount:cloudsql-access@maximal-relic-457716-k6.iam.gserviceaccount.com" \
+  --role="roles/storage.objectViewer" \
+  --condition=None
+
+gsutil cp gs://maximal-relic-war-artifacts-k6/wars/TaskManager.war TaskManager.war
+
+docker cp TaskManager.war my-tomcat-container:/usr/local/tomcat/webapps/
+
+docker restart my-tomcat-container
